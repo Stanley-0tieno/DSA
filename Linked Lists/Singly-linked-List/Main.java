da1ad00acbcab4385fee2ad9750784c5f41218d0
startMrSnow <175dollarsnow@gmail.com>
2025-11-18 12:52:47 +0300

    Change: New Linked list

--- Linked Lists/Singly-linked-List/Main.java
+++ Linked Lists/Singly-linked-List/Main.java
@@ -1,34 +1,27 @@
+
 class Node{
     int data;
     Node next;
 
-    //constructor to initialize a new node
-    Node(int new_data){
-        this.data = new_data;
+    Node(int new_int){
+        this.data = new_int;
         this.next = null;
+
     }
 }
-
 public class Main{
-    public static void main(String[] args){
-
-        //create the first node(head of the list)
+    public static void main (String[] args){
         Node head = new Node(10);
 
-        //link the second node
         head.next = new Node(20);
 
-        //link the third node
-        head.next.next = new Node(40);
+        head.next.next = new Node(30);
 
-        head.next.next.next = new Node(50);
-
-        //print linked list
         while(head != null){
             System.out.println(head.data + " ");
-            System.out.println(head.next + " ");
             head = head.next;
         }
+
 
     }
 }
