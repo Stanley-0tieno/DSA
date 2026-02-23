3aa55a649ee10f5e77c1adbda7d5296718faebe0
startMrSnow <175dollarsnow@gmail.com>
2026-02-23 10:23:36 +0300

    change: next roman number

--- PY/Roman_to_integer.py
+++ PY/Roman_to_integer.py
@@ -10,18 +10,33 @@ def Solution(x):
     }
 
     total = 0
-    prev_value = 0
+    # prev_value = 0
+    
+    # for char in reversed(x):
+    #     value = roman_map[char]
+    #     if value < prev_value:
+    #         total -= value
 
-    for char in reversed(x):   # read from right → left
-        value = roman_map[char]
+    #     else :
+    #         total += value
 
-        if value < prev_value:
-            total -= value
+    # print(total)
+
+    for i in range(len(x)):
+        value = roman_map[x[i]]
+
+        if i + 1 < len(x):
+            next_value = roman_map[x[i+1]]
+
+            if value < next_value:
+                total -= value
+
+            else:
+                total += value
         else:
             total += value
 
-        prev_value = value
+        print(total)
 
-    print(total)
-    
-Solution("XI")
+
+Solution("X")
